01f8876c421b869f62b3989120ed6a48c6136c4e
Aibarret <77796971+Aibarret@users.noreply.github.com>
2021-03-27 23:07:22 -0400

    Finished assignment

---
+++ Enemy/Enemy_Ground.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+export var speed = 100
+export var damage = 10
+export var max_constraint = 100
+export var min_constraint = 100
+var direction = 1
+var velocity = Vector2.ZERO
+
+func _physics_process(_delta):
+	if direction < 0 and !$Sprite.flip_h:
+		$Sprite.flip_h = true
+	if direction > 0 and !$Sprite.flip_h:
+		$Sprite.flip_h = false
+	if direction > 0 and position.x >= max_constraint:
+		velocity.x = 0
+		direction = -1
+	if direction < 0 and position.x <= min_constraint:
+		velocity.x = 0
+		direction = 1
+	velocity.x += direction * speed
+	move_and_slide_with_snap(velocity, Vector2.UP)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		if collision.collider.name == "Ground" or collision.collider.name == "Platform":
+			velocity.x = 0
+			direction *= -1
+		if collision.collider.name == "Player":
+			collision.collider.do_damage(damage)
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Ground" or body.name == "Platform":
+		velocity.x = 0
+		direction *= -1
+	if body.name == "Player":
+		body.do_damage(damage)
---
+++ Hud/HUD.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+
+func _physics_process(delta):
+	$Score.text = "Score: " + str(Global.score)
+	$Lives.text = "Lives: " + str(Global.lives)
+	$Health.text = "Health: " + str(Global.health)
